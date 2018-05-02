41966c64e025fe91560e01ed76544f127f7b721a
Nivose44 <rayaneberrada44500@gmail.com>
2018-05-03 00:30:58 +0200

    first template created

--- OpenFoodFacts_project/aliments_manager/views.py
+++ OpenFoodFacts_project/aliments_manager/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
+    return render(request, 'aliments_manager/index.html')
