ebb58554e1a3053a90df80ea55820c88b0897721
Nivose44 <rayaneberrada44500@gmail.com>
2018-05-05 16:46:52 +0200

    First functionnality can now search and display aliments images from OFF

--- OpenFoodFacts_project/aliments_manager/views.py
+++ OpenFoodFacts_project/aliments_manager/views.py
@@ -1,5 +1,8 @@
+import requests
+import json
 from django.shortcuts import render
 from .forms import ContactForm
+
 
 # Create your views here.
 def home(request):
@@ -8,3 +11,19 @@ def home(request):
     if form.is_valid(): 
     	succes = True 
     return render(request, 'aliments_manager/index.html', locals())
+
+def results(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid(): 
+    	succes = True 
+    	sujet = form.cleaned_data['sujet']
+    products_to_get = {'search_terms':sujet, 'page_size':6, 'page':1, 'json':1}
+    request_api_products = requests.get('https://fr.openfoodfacts.org/cgi/search.pl', params = products_to_get)
+    json = request_api_products.json()
+    products = json['products']
+    aliments = []
+    for product in products:
+        aliment = {"name":product['product_name'], "url":product['image_url'], "grade":product['nutrition_grades']}
+        aliments.append(aliment)
+    print(aliments)
+    return render(request, 'aliments_manager/results.html', locals())
