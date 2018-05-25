5839f212e1265f8bbc59cfaba5564b8e01b31da8
Nivose44 <rayaneberrada44500@gmail.com>
2018-05-25 02:44:37 +0200

    ready for heroku

---
+++ aliments_manager/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    sujet = forms.CharField(max_length=100, label='')
+
+class RegistrationForm(forms.Form):
+	nameUser = forms.CharField(max_length=8)
+	email = forms.EmailField()
+	password = forms.CharField(max_length=12, widget=forms.PasswordInput) 
+
+class ConnectionForm(forms.Form):
+	nameUser = forms.CharField(max_length=8)
+	password = forms.CharField(max_length=32, widget=forms.PasswordInput) 
+	
