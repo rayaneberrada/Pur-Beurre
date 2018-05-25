85a46192be78f6c6881c847b83db2835840b8c63
Nivose44 <rayaneberrada44500@gmail.com>
2018-05-25 11:27:02 +0200

    pagination update

--- aliments_manager/urls.py
+++ aliments_manager/urls.py
@@ -4,11 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('results/<str:aliment_searched>/<int:page_id>/', views.results, name='results'),
-    path('registration', views.registration, name='registration'),
-    path('connection', views.connection, name='connection'),
-    path('disconnection', views.disconnection, name='disconnection'),
-    path('account', views.account, name='account'),
-   	path('add_favorite', views.add_favorite, name='add_favorite'),
+    path('registration/', views.registration, name='registration'),
+    path('connection/', views.connection, name='connection'),
+    path('disconnection/', views.disconnection, name='disconnection'),
+    path('account/', views.account, name='account'),
+   	path('add_favorite/', views.add_favorite, name='add_favorite'),
    	path('favorites/<int:page_id>/', views.show_favorites, name='favorites' ),
    	path('aliment/<str:code>/', views.show_aliment, name='aliment')
 ]
